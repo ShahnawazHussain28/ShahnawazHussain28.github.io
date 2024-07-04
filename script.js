6f7e3c788195df1881ad1a0ba00f302b4fa023a2
Shahnawaz Hussain <shahnawazhussain2802@gmail.com>
2024-07-04 16:23:53 +0530

    fix: added better redirection

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 let contentDiv = document.getElementById("content");
 let contentHTML = "";
 let data = [];
-let query = window.location.search;
-if (query.includes("gallery")) {
-  console.log(query);
-} else {
-  console.log("redirect");
-  window.location.replace(
-    "https://portfolio-shahnawazhussain28s-projects.vercel.app/",
-  );
-}
 
 function getDOM(link, thumbLink, name, description) {
   let dom = `<div class='services' onclick="window.location.href ='${link}'">
